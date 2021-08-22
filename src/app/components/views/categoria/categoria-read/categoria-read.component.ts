57bbbef7445208681c9523684dcb09ce4c2bea72
WesleDev <wesle.profissional@gmail.com>
2021-08-22 20:02:54 -0300

    categoria-read implementado

---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../../categoria.service';
+import { Categoria } from '../categorial.model';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css'],
+})
+export class CategoriaReadComponent implements OnInit {
+  categorias: Categoria[] = [];
+
+  displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
+
+  constructor(private service: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe((resposta) => {
+      console.log(resposta);
+      this.categorias = resposta;
+    });
+  }
+}
