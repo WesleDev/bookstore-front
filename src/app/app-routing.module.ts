ae70ecffbeeaf72245c770b1445e80ca8b90c62f
WesleDev <wesle.profissional@gmail.com>
2021-09-02 19:23:17 -0300

    read para livro implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './components/views/home/home.component';
 import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
 import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 import { LivroDeleteComponent } from './components/views/livro/livro-delete/livro-delete.component';
+import { LivroReadComponent } from './components/views/livro/livro-read/livro-read.component';
 
 const routes: Routes = [
   {
@@ -46,6 +47,10 @@ const routes: Routes = [
   {
     path: 'categorias/:id_cat/livros/:id/delete',
     component: LivroDeleteComponent,
+  },
+  {
+    path: 'categorias/:id_cat/livros/:id/read',
+    component: LivroReadComponent,
   },
 ];
 
